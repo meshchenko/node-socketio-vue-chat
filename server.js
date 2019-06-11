f25f47e5d59534525d7f41518bd0c4e8dffe464c
mikhail.eshchenko <mikhail.eshchenko@gmail.com>
2019-06-11 12:10:11 +0300

    showing user as they connect

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const app = require('express')();
 const http = require('http').Server(app); //let us to listen port and callback when we start server
-let io = require('socket.io')(http);
+const io = require('socket.io')(http);
 
 app.get('/', function (request, response) {
   response.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection',function(){
-  console.log('Someone joined');
-})
+io.on('connection', function (socket) {
+  let user = Date.now();
+
+  socket.broadcast.emit('message', `User ${user} connected`); //show to anyone exept new user
+});
 
 http.listen(3000, function () {
   console.log('Started server');
-})
+});
