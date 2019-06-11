1406fe67966f7d941491453a24331b5d759bef72
mikhail.eshchenko <mikhail.eshchenko@gmail.com>
2019-06-11 10:39:42 +0300

    setting up Socket.io

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const app = require('express')();
 const http = require('http').Server(app); //let us to listen port and callback when we start server
+let io = require('socket.io')(http);
 
 app.get('/', function (request, response) {
   response.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection',function(){
+  console.log('Someone joined');
+})
 
 http.listen(3000, function () {
   console.log('Started server');
